185e4d0469de63a30515cccb9701d286523513ed
ultimategamer309 <50726496+ultimategamer309@users.noreply.github.com>
2021-04-13 15:51:41 -0600

    Add files via upload

--- Python Installation/Mac/python.sh
+++ Python Installation/Mac/python.sh
@@ -1,2 +1,9 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-homebrew install python3
+if ! command -v brew &>/dev/null; then
+  echo "Installing Homebrew, an OSX package manager, follow the instructions..." 
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+else
+  echo "You already have Homebrew installed...good job!"; fi
+
+echo "Installing Python"
+brew install python3
